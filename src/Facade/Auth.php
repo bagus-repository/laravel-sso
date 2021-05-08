9dbd4263238ceb7cfd2be3c854f565b5f8ba95b5
ersadishla <ersadishlahuddin@gmail.com>
2021-05-08 13:07:55 +0700

    update login logout parameter

--- src/Facade/Auth.php
+++ src/Facade/Auth.php
@@ -7,16 +7,16 @@ namespace Dptsi\Sso\Facade;
 use Illuminate\Support\Facades\Facade;
 
 /**
- * Class ModuleManager
+ * Class Sso
  * @package Dptsi\Sso\Facade
- * @method static void loginSso(OidcRequest $request)
- * @method static void logoutSso()
- * @method static ?bool check()
- * @method static ?\Dptsi\Sso\Models\User user()
+ * @method static void loginSso(\Dptsi\Sso\Requests\OidcLoginRequest $request)
+ * @method static void logoutSso(\Dptsi\Sso\Requests\OidcLogoutRequest $request)
+ * @method static bool check()
+ * @method static \Dptsi\Sso\Models\User user()
  * @method static void set(\Dptsi\Sso\Models\User $user)
- * @method static ?string token()
- * @method static ?array roles()
- * @method static ?array activeRole()
+ * @method static string token()
+ * @method static array roles()
+ * @method static array activeRole()
  */
 
 class Auth extends Facade
